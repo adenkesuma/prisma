dfc3db4eb7f6708d3801ed3c267d5fb389975e5e
adenkesuma <adenfdfd10@gmail.com>
2023-06-06 20:03:10 +0700

    learn use every way to use prisma

--- script.ts
+++ script.ts
@@ -31,12 +31,80 @@ async function main() {
   })
   
   */
-
+  /*
   const user = await prisma.user.findUnique({
     where: {
       age_name: {
         age: 20,
         name: "asengdev"
+      }
+    }
+  })
+  */
+
+  /*const user = await prisma.user.findFirst({
+    where: {
+      name: "pedro"
+    }
+  }) */
+
+  /*
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: "pedro2",
+        email: "pedro2@gmail.com",
+        age: 22,
+        isAdmin: false
+      },
+      {
+        name: "pedro3",
+        email: "pedro3@gmail.com",
+        age: 23,
+        isAdmin: false
+      },
+      {
+        name: "pedro4",
+        email: "pedro4@gmail.com",
+        age: 24,
+        isAdmin: false
+      }
+    ]
+  })
+  */
+  /*
+  const users = await prisma.user.findMany({
+    where: {
+      name: "pedro",
+      // lt berarti data umur yang dibawah dari 19
+      age: { lt: 19 }
+    },
+    orderBy: {
+      age: "desc"
+    },
+    take: 2,
+    skip: 1
+  })
+  */  
+ /* 
+  const users = await prisma.user.findMany({
+    where: {
+      name: { contains: "pedro" },
+      AND: [
+        { email: { startsWith: "pedro"}},
+        { email: { endsWith: "@gmail.com"}}
+      ] 
+    }
+  })
+  */
+  
+  const user = await prisma.user.update({
+    where: {
+      email: "asengdev@gmail.com"
+    },
+    data: {
+      age: {
+        multiply: 10
       }
     }
   })
