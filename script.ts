9e7557ba867fdc2068f5d05d5a8793102948bb90
adenkesuma <adenfdfd10@gmail.com>
2023-06-06 18:42:59 +0700

    learn how to use delete, createMany, and find many

--- script.ts
+++ script.ts
@@ -5,8 +5,43 @@ const prisma = new PrismaClient()
 async function main() {
  // const user = await prisma.user.create({ data: { name: "marsya" }})
  // const users = await prisma.user.findMany()
-  const users = await prisma.user.deleteMany()
-  console.log(users)
+ // const users = await prisma.user.deleteMany()
+ /* 
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: "aden",
+        email: "aden@gmail.com",
+        age: 18,
+        isAdmin: true
+      },
+      {
+        name: "asengdev",
+        email: "asengdev@gmail.com",
+        age: 20,
+        isAdmin: false,
+      },
+      {
+        name: "pedro",
+        email: "pedro@gmail.com",
+        age: 23,
+        isAdmin: false
+      }
+    ],
+  })
+  
+  */
+
+  const user = await prisma.user.findUnique({
+    where: {
+      age_name: {
+        age: 20,
+        name: "asengdev"
+      }
+    }
+  })
+
+  console.log(user)
 }
 
 main()
